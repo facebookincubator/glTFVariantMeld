cde9e978ed2de0786719d2e6127ceda5ddb977e7
Pär Winzell <zell@fb.com>
2019-10-05 17:57:31 -0500

    Zell/travis support (#9)
    This adds a simple .travis.yml file to smoke-test builds. To make things easier for Travis, and to appease other Rust tools that expect a standard directory layout right in the Git root, we're adding a workspace-style Cargo.toml at the root.

--- native/build-wasm-pkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-#
-
-SCRIPT_DIR=`dirname $BASH_SOURCE`
-cd "${SCRIPT_DIR}"
-
-wasm-pack build -d ${SCRIPT_DIR}/../web/wasmpkg/
